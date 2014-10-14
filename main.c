95558da0c837ea7c0de14546e899e5dcbc28daaa
cosc301 <cgrace@colgate.edu>
2014-10-14 18:39:14 -0400

    update639

--- main.c
+++ main.c
@@ -63,146 +63,146 @@ char** tokenify(char input []) {
 int main(int argc, char **argv) {
 
 	int mode = 0; // 0 for sequential, 1 for parallel
-	bool done = true;
-	int exit = 0; // specifies if we should exit at end of parallel execution 
+	int exit_later = 0; // specifies if we should exit at end of parallel execution 
 	int new_mode = 0; // 0 means no change, 1 means to S, 2 means to P
 	char ** ptrs;
+	bool done = false;
+
+	while (!done) {
 
 
-	char input[1024];
-	printf("This is the prompt: ");
-	fflush(stdout);
+		char input[1024];
+		printf("This is the prompt: ");
+		fflush(stdout);
 
-	while (fgets(input, 1024, stdin)!= NULL) {
+		while (fgets(input, 1024, stdin)!= NULL) {
 	
-		input[strlen(input)-1] = '\0';
 
-		// exit if input only says EOF or exit
-		if (check_exit(input)==true) { exit();} // must pass const char * here
-
-		// ignore comments; replace all # with null termination
-		replace_hash(input);
-
-		// parse the command and organize the separate commands in memory
-		ptrs = tokenify(input); // array of char pointers to each different token (commands will always start with /bin)
-
-
-
-		if (mode ==0) {
-			const char * mode_str="mode";
-			const char * par="p";
-			const char * seq= "s";
-			char** temp= ptrs;
-			int i =0;
-			
-			pid_t child_pid;
-			while (temp[i]!=NULL) {
-				if ( strstr(temp[i], mode_str) !=NULL) {
-					if (strstr(temp[i],seq)!=NULL) {new_mode=1;}
-					if (strstr(temp[i],par)!=NULL) {new_mode=2;}
-					i++;
-					continue;
-					}
-				if ( check_exit(temp[i])==true) {
-					exit(0);
-					}
-				if ((child_pid = fork() ) ==0) { 
-					//if in child
-					if (execv(temp[i], temp) < 0) {
-        						printf("execv failed");
-    						}
-					}
-				else {
-					//if parent wait
-					wait(); 
-					}
-				i++; //increment
-
-				}
-			
-			
-			// create new process for each new command
-			// change mode 
-			if (new_mode==1) {mode=0;}
-			if (new_mode==2) {mode=1;}
-		}
-			
 		
-		
-		// when in parallel mode...
-		// check for exit command in parallel mode and mode switch in parallel
-		char ** temp = ptrs;
-		int i = 0;
-		const char * mode_word = "mode";
-		const char * exit_word = "exit";
-		const char * seq = "s";
-		const char * par = "p";
-		if (mode ==1) {
+			input[strlen(input)-1] = '\0';
+			const char *inputptr = input;
+			// exit if input only says EOF or exit
+			if (check_exit(inputptr)==true) { exit(0);} // must pass const char * here
+
+			// ignore comments; replace all # with null termination
+			replace_hash(input);
+
+			// parse the command and organize the separate commands in memory
+			ptrs = tokenify(input); // array of char pointers to each different token (commands will always start with /bin)
+
+	////////////////////////////////////////////////////////////////////////
+			const char * mode_word = "mode";
+			const char * exit_word = "exit";
+			const char * seq = "s";
+			const char * par = "p";
+	//////////////////////////////////////////////
+			if (mode ==0) {
+				char** temp1= ptrs;
+				int i =0;
 			
-			// loop through all tokens to find mode or exit changes
-			while (temp[i] != NULL) {
-		
-				if (strstr(temp[i], mode_word) != NULL) {
-					// means we need to change mode at the end
-					if (strstr(temp[i], seq)) {
-						new_mode = 1;
-					}
-					else if (strstr(temp[i], par)) {
-						new_mode = 2;
-					}
-				}
-
-				if (strstr(temp[i], exit_word)!=NULL) {
-					exit = 1;	
-				}
-				i++;		
-			}
-
-			i = 0;
-
-			while (ptrs[i] !=NULL) {
-				// if it's NOT mode or exit, then...
-				if (strstr(ptrs[i], mode_word)==NULL && strstr(ptrs[i], exit_word)==NULL) {
-					pid_t child = fork();
-
-					if (child ==0) {
-						execv(ptrs[i]); // HOW do i call execv?
-					}
-				
+				pid_t child_pid;
+				while (temp1[i]!=NULL) {
+					if ( strstr(temp1[i], mode_word) !=NULL) {
+						if (strstr(temp1[i],seq)!=NULL) {new_mode=1;}
+						if (strstr(temp1[i],par)!=NULL) {new_mode=2;}
+						i++;
+						continue;
+						}
+					if ( check_exit(temp1[i])==true) {
+						exit(0);
+						}
+					if ((child_pid = fork() ) ==0) { 
+						//if in child
+						if (execv(temp1[i], temp1) < 0) {
+		    						printf("execv failed");
+								}
+						}
 					else {
-						wait(); 
+						//if parent wait
+						wait(NULL); 
+						}
+					i++; //increment
 					}
-				}
-				i++;
+
+				// change mode 
+				if (new_mode==1) {mode=0;}
+				if (new_mode==2) {mode=1;}
 			}
+			
+	///////////////////////////////////////////////////////////////////////////////////
+		
+			// when in parallel mode...
+			// check for exit command in parallel mode and mode switch in parallel
+			char ** temp = ptrs;
+			int i = 0;
+
+			if (mode ==1) {
+			
+				// loop through all tokens to find mode or exit changes
+				while (temp[i] != NULL) {
+		
+					if (strstr(temp[i], mode_word) != NULL) {
+						// means we need to change mode at the end
+						if (strstr(temp[i], seq)) {
+							new_mode = 1;
+						}
+						else if (strstr(temp[i], par)) {
+							new_mode = 2;
+						}
+					}
+
+					if (strstr(temp[i], exit_word)!=NULL) {
+						exit_later = 1;	
+					}
+					i++;		
+				}
+
+				i = 0;
+
+				while (ptrs[i] !=NULL) {
+					// if it's NOT mode or exit, then...
+					if (strstr(ptrs[i], mode_word)==NULL && strstr(ptrs[i], exit_word)==NULL) {
+						pid_t child = fork();
+
+						if (child ==0) {
+							execv(ptrs[i], ptrs); // HOW do i call execv?
+						}
+				
+						else {
+							wait(NULL); 
+						}
+					}
+					i++;
+				}
 
 				
-			// do mode and exit stuff now
+				// do mode and exit stuff now
 
-			if (new_mode ==1 ) {
-				mode = 0; // update it to sequential
-			}
-			else if (new_mode ==2) {
-				mode = 1; // update it to parallel
-			}
+				if (new_mode ==1 ) {
+					mode = 0; // update it to sequential
+				}
+				else if (new_mode ==2) {
+					mode = 1; // update it to parallel
+				}
 
-			if (exit == 1) {
-				exit();
+				if (exit_later == 1) {
+					exit(0);
+				}
 			}
 		}
-	}
 
 
 
 
 
-	// fgets returned NULL, so check for EOF
-	if (feof(stdin)) {
-		exit(0); // exit if there's EOF 
-	}
+		// fgets returned NULL, so check for EOF
+		if (feof(stdin)) {
+			exit(0); // exit if there's EOF 
+		}
 	
-	free(ptrs);
+		free(ptrs);
 
-    return 0;
+		return 0;
+	}
 }
 
