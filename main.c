25c6d73cd329e330ebb3b8d3fcd412e794640d2a
cosc301 <cgrace@colgate.edu>
2014-10-14 21:50:54 -0400

    update950

--- main.c
+++ main.c
@@ -12,6 +12,22 @@
 #include <signal.h>
 #include <stdbool.h>
 
+void removewhitespace(char *s) {
+
+	// move through the c string with reading and writing markers
+	int r = 0;
+	int w = 0;
+	while (r < strlen(s)) {
+		if (!isspace(s[r])) {
+			s[w] = s[r]; // copy char
+			r++; 	
+			w++; 	
+		}
+		else {r++;} // advance reader if space is found	
+	}
+	s[w] = '\0';
+}
+
 
 void replace_hash (char arr []) {
 	for (int i = 0; i<strlen(arr); i++) {
@@ -31,7 +47,7 @@ bool check_exit (const char * arr) {
 }
 
 
-char** tokenify(char input []) {
+char** tokenify_semicolon(char input []) {
 	char *ptr_input = input;
 	char *pstring = strdup(ptr_input); //pointer to copy of input 
 	char *ptoken; 
@@ -53,6 +69,39 @@ char** tokenify(char input []) {
 	return array;
 }
 
+char** tokenify_space(char input []) {
+	char *ptr_input = input;
+	char *pstring = strdup(ptr_input); //pointer to copy of input 
+	char *ptoken; 
+	ptoken = strtok(pstring, " \t\n"); // ptoken now points to first token
+	char **array = malloc(sizeof(char*)*((strlen(ptr_input)/2)+2));
+	
+	// count the number of tokens
+	int i = 0;
+	while (ptoken != NULL) {
+		char *ptrtok = strdup(ptoken);
+		array[i] = ptrtok;
+		ptoken = strtok(NULL, " \t\n"); // get each next token
+		i++;	
+	}
+	// set last index in array to NULL
+	array[i]=NULL;
+	// free 
+	free(pstring);
+	return array;
+}
+
+bool check_mode(char* input) {
+	char * mode = "mode";
+
+	removewhitespace(input);
+	if ((strcmp(mode, input))==0) {
+		return true;
+	}
+	return false;
+
+}
+
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -72,10 +121,11 @@ int main(int argc, char **argv) {
 
 
 		char input[1024];
-		printf("This is the prompt: ");
+		char * input_dup = input;
+		printf("prompt: ");
 		fflush(stdout);
 
-		while (fgets(input, 1024, stdin)!= NULL) {
+		if (fgets(input, 1024, stdin)!= NULL) {
 	
 
 		
@@ -84,17 +134,29 @@ int main(int argc, char **argv) {
 			// exit if input only says EOF or exit
 			if (check_exit(inputptr)==true) { exit(0);} // must pass const char * here
 
+			if (check_mode(input_dup)==true) {
+
+				if (mode ==0) {
+					printf("Current mode is sequential\n");
+				}
+
+				else if (mode==1) {
+					printf("Current mode is parallel\n"); 
+				}
+			}
+
 			// ignore comments; replace all # with null termination
 			replace_hash(input);
 
 			// parse the command and organize the separate commands in memory
-			ptrs = tokenify(input); // array of char pointers to each different token (commands will always start with /bin)
+			ptrs = tokenify_semicolon(input); // array of char pointers to each different token (commands will always start with /bin)
 
 	////////////////////////////////////////////////////////////////////////
 			const char * mode_word = "mode";
 			const char * exit_word = "exit";
 			const char * seq = "s";
 			const char * par = "p";
+			char** command;
 	//////////////////////////////////////////////
 			if (mode ==0) {
 				char** temp1= ptrs;
@@ -111,15 +173,20 @@ int main(int argc, char **argv) {
 					if ( check_exit(temp1[i])==true) {
 						exit(0);
 						}
-					if ((child_pid = fork() ) ==0) { 
+					child_pid = fork();
+					if (child_pid ==0) { 
 						//if in child
-						if (execv(temp1[i], temp1) < 0) {
-		    						printf("execv failed");
-								}
+						command = tokenify_space(*temp1);
+						if (execv(command[0], command) < 0) {
+		    						printf("execv failed\n");
+						}
+						exit(0);
+					
 						}
 					else {
 						//if parent wait
-						wait(child_pid); 
+						int return_status;
+						waitpid(child_pid, &return_status, 0); 
 						}
 					i++; //increment
 					}
@@ -169,7 +236,7 @@ int main(int argc, char **argv) {
 						}
 				
 						else {
-							wait(child); 
+							waitpid(child, NULL, -1); 
 						}
 					}
 					i++;
@@ -191,10 +258,6 @@ int main(int argc, char **argv) {
 			}
 		}
 
-
-
-
-
 		// fgets returned NULL, so check for EOF
 		if (feof(stdin)) {
 			exit(0); // exit if there's EOF 
@@ -202,7 +265,7 @@ int main(int argc, char **argv) {
 	
 		free(ptrs);
 
-		return 0;
 	}
+	return 0;
 }
 
