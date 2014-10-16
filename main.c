81697c1d3f99ef37c2c2c338cfab3ec4e16d6fd0
cosc301 <cgrace@colgate.edu>
2014-10-16 19:15:33 -0400

    updated715

--- main.c
+++ main.c
@@ -1,3 +1,6 @@
+//Claire Grace 
+//Ethan Jones
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -18,7 +21,7 @@ struct node {
 
 };
 
-
+/* plan for background job capability 
 struct node2 {
 	pid_t pid;
 	char * process;
@@ -26,7 +29,7 @@ struct node2 {
 	struct node2 *next;
 
 };
-
+*/
 
 void removewhitespace(char *s) {
 
@@ -80,9 +83,19 @@ char** tokenify_semicolon(char input []) {
 	}
 	// set last index in array to NULL
 	array[i]=NULL;
-	// free 
 	free(pstring);
 	return array;
+}
+
+
+void free_tokens(char **tokens) {
+    int i = 0;
+	char ** temp = tokens;
+    while (tokens[i] != NULL) {
+        free(tokens[i]); // free each string
+        i++;
+    }
+    free(temp); // then free the array
 }
 
 char** tokenify_space(char *input) {
@@ -123,50 +136,73 @@ void list_append(char* name, struct node **head) { // taken from hw03
 	struct node *newnode = malloc(sizeof(struct node));
 	newnode->word = name;
 	newnode->next = NULL;
-
 	if (*head == NULL) { // if list is empty, set it to new node
 		*head = newnode; 
-		
 		return;
-		
 	}
-
 	struct node *temp = *head;
-
-	
 	while (temp->next != NULL) {
 		temp = temp->next;
 	}
-	
 	temp->next = newnode;
 	return;
 }
 
 
+void print_mode_status(int mode) {
+	if (mode ==0) {
+		printf("Current mode is sequential\n");
+	}
+
+	else if (mode==1) {
+		printf("Current mode is parallel\n"); 
+	}	
+	return;
+}
+
+void update_mode(int new_mode, int * modep) {
+		if (new_mode==1) {*modep=0;}
+		if (new_mode==2) {*modep=1;}
+		return;
+}
+
+void free_LL(struct node *head) { 
+    while (head != NULL) {
+		struct node *temp = head;
+		struct node *next = head->next;
+        free(temp); // free each node
+        head = next;
+    }
+	free(head);
+    return;
+
+}
+
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
 
 int main(int argc, char **argv) {
 
 	int mode = 0; // 0 for sequential, 1 for parallel
+	int * modep = &mode;
 	int exit_later = 0; // specifies if we should exit at end of parallel execution 
 	int new_mode = 0; // 0 means no change, 1 means to S, 2 means to P
 	char ** ptrs;
 	bool done = false;
 	char *newnewstring = malloc(sizeof(char)*1024);
-
 	struct node *head = malloc(sizeof(struct node));
-
-    FILE *fp = NULL;
+	const char * mode_word = "mode";
+	const char * exit_word = "exit";
+	const char * seq = "s";
+	const char * par = "p";
+	char** command;
+	FILE *fp = NULL;
 	fp = fopen ("shell-config", "r");
 	char line[1024];
 	char * linep = line;
 	char * newp;
-
-	
+	char newstr[1024];
+	char *newstring=newstr;
 
 	while(fgets(line, 128, fp) != NULL)
    {
@@ -176,19 +212,15 @@ int main(int argc, char **argv) {
 		newp = malloc(sizeof(char)*1024);
 		strcpy(newp, linep);
 		char slash[] = "/";
-		char * slashp = slash;
-		
+		char * slashp = slash; // add a slash to the end of every line in shell-config
 		newp=strcat(newp, slashp);
 
 		//then append to the list 
 		list_append(newp, &head);
-	
-
    }
+	//free(newp);
 
 	struct node *tempnode = head->next;
-
-
 	while (!done) {
 
 
@@ -200,50 +232,26 @@ int main(int argc, char **argv) {
 
 		if (fgets(input, 1024, stdin)!= NULL) {
 	
-
-		
 			input[strlen(input)-1] = '\0';
-			// ignore comments; replace all # with null termination
-			replace_hash(input);
-
-			//const char *inputptr = input;
-
-
-			// parse the command and organize the separate commands in memory
-			ptrs = tokenify_semicolon(input); 
-
+			replace_hash(input); // ignore comments; replace all # with null termination
+			ptrs = tokenify_semicolon(input); // parse the command and organize the separate commands in memory
+		
 
 			if (strstr(input_dup, mode_check)!=NULL) {
 				if (check_mode(input_dup)==true) {
-
-					if (mode ==0) {
-						printf("Current mode is sequential\n");
-					}
-
-					else if (mode==1) {
-						printf("Current mode is parallel\n"); 
-					}	
+					print_mode_status(mode);
 				}
 			}
 
-			
-
-	////////////////////////////////////////////////////////////////////////
-			const char * mode_word = "mode";
-			const char * exit_word = "exit";
-			const char * seq = "s";
-			const char * par = "p";
-			char** command;
-	//////////////////////////////////////////////
+	// if in sequential mode: 
 			if (mode ==0) {
 				char** temp1= ptrs;
-				
 				int i =0;
 
 				pid_t child_pid;
 				while (temp1[i]!=NULL) {
 
-					if ( strstr(temp1[i], mode_word) !=NULL) {
+					if (strstr(temp1[i], mode_word) !=NULL) {
 						if (strstr(temp1[i],seq)!=NULL) {new_mode=1;}
 						if (strstr(temp1[i],par)!=NULL) {new_mode=2;}
 						i++;
@@ -253,11 +261,8 @@ int main(int argc, char **argv) {
 						exit(0);
 					}
 
-
 					child_pid = fork();
-
-					if (child_pid ==0) { 
-						//if in child
+					if (child_pid ==0) {  // if in child
 						command = tokenify_space(temp1[i]);
 						
 						if (command[0] == NULL) {
@@ -266,55 +271,45 @@ int main(int argc, char **argv) {
 						}
 
 						// path variable capability
-						
 						struct stat statresult;
 						int rv = stat(command[0], &statresult);
 						char *cmdcopy=command[0];
-						char newstr[1024];
-						newstr[0] = '\0';
-						char *newstring=newstr;
-
-						if (rv<0){
-					
-							while(tempnode->next!=NULL) {
+						newstr[0] = '\0';	
 						
+	
+						if (rv<0){
+							while(tempnode->next!=NULL) {
 								newstring=strcat(newstring,tempnode->word);
 								newstring=strcat(newstring,cmdcopy);
-							
 								rv= stat(newstring,&statresult);
 					
-						
 								if (rv==0) {
-								
 									strcpy(newnewstring, newstring);
 									command[0] = newnewstring;
-							
 									break;
 								}
 								tempnode=tempnode->next;
 							}
 						}
 
-
-		
 						if (execv(command[0], command) < 0) {
 		    						printf("execv failed\n");
 						}
+						//free_tokens(command);
 						exit(0);
 					
 					}
+					
 					else {
 						//if parent wait
 						int return_status;
 						waitpid(child_pid, &return_status, 0); 
 						i++;	
 					}
-				
 				}
 
 				// change mode 
-				if (new_mode==1) {mode=0;}
-				if (new_mode==2) {mode=1;}
+				update_mode(new_mode, modep);
 			}
 			
 	///////////////////////////////////////////////////////////////////////////////////
@@ -328,7 +323,6 @@ int main(int argc, char **argv) {
 			
 				// loop through all tokens to find mode or exit changes
 				while (temp[i] != NULL) {
-		
 					if (strstr(temp[i], mode_word) != NULL) {
 						// means we need to change mode at the end
 						if (strstr(temp[i], seq)) {
@@ -338,7 +332,7 @@ int main(int argc, char **argv) {
 							new_mode = 2;
 						}
 					}
-
+					
 					if (strstr(temp[i], exit_word)!=NULL) {
 						exit_later = 1;	
 					}
@@ -346,18 +340,15 @@ int main(int argc, char **argv) {
 				}
 
 				i = 0;
-
 				while (ptrs[i] !=NULL) {
 					// if it's NOT mode or exit, then...
 					if (strstr(ptrs[i], mode_word)==NULL && strstr(ptrs[i], exit_word)==NULL) {
-						pid_t child = fork();
-						
+						pid_t child = fork();						
 
 						if (child ==0) {
 
 							pid_t child2 = fork();
 							if (child2 == 0) {
-							
 								command = tokenify_space(temp[i]);
 
 								if (command[0] == NULL) {
@@ -365,98 +356,77 @@ int main(int argc, char **argv) {
 									continue;
 								}	
 
-
 								// path variable capability
-						
 								struct stat statresult;
 								int rv = stat(command[0], &statresult);
 								char *cmdcopy=command[0];
-								char newstr[1024];
 								newstr[0] = '\0';
-								char *newstring=newstr;
-
+							
 								if (rv<0){
 					
 									while(tempnode->next!=NULL) {
-						
 										newstring=strcat(newstring,tempnode->word);
 										newstring=strcat(newstring,cmdcopy);
-							
 										rv= stat(newstring,&statresult);
-					
-						
 										if (rv==0) {
-								
 											strcpy(newnewstring, newstring);
 											command[0] = newnewstring;
-							
 											break;
 										}
 										tempnode=tempnode->next;
 									}
 								}
 
-
-
 								if (execv(command[0], command) < 0) {
 									printf("execv failed\n");
 								}
+								//free_tokens(command);
 								exit(0);
 							}
 								
+							
 							else {
 								int return_status;
 								waitpid(child2, &return_status, 0);
-								
 								printf("Process %i has completed", child2);
 								exit(0);
-
 							}						
-		
 						}
 				
 						else {
-						
 							//int return_status;
-							//waitpid(child, &return_status, 0);
-							
+							//waitpid(child, &return_status, 0);	
 						}
 					}
 					i++;
-					
 				}
 
-				
-				// do mode and exit stuff now
-
+				// handle mode and exit now
 				if (new_mode ==1 ) {
 					mode = 0; // update it to sequential
 				}
 				else if (new_mode ==2) {
 					mode = 1; // update it to parallel
 				}
-
 				if (exit_later == 1) {
 					exit(0);
 				}
-				}
 			}
-
+		
+		}
 		
 
 		// fgets returned NULL, so check for EOF
 		if (feof(stdin)) {
-			fclose(fp);
-		
-			free(newnewstring);
-			free(ptrs);
-		
-
 			exit(0); // exit if there's EOF 
 		}
-	
 		
 	}
+	fclose(fp);
+	//free_tokens(command);
+	free(newnewstring);
+	//free_LL(head);
+	free(head);		
 	return 0;
 }
 
